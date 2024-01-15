0b94edc234836633c221aeecf63b6188f7224312
Dmitrius Agoston <dmitri.agoston@gmail.com>
2024-01-15 15:21:37 -0800

    find source and destination box

--- src/nm_pathfinder.py
+++ src/nm_pathfinder.py
@@ -13,13 +13,35 @@ def find_path (source_point, destination_point, mesh):
         A path (list of points) from source_point to destination_point if exists
         A list of boxes explored by the algorithm
     """
-    print(mesh)
     path = []
     boxes = {}
 
+    # source point x and y cords
+    spx = source_point[0]
+    spy = source_point[1]
+
+    # destination point x and y cords
+    dpx = destination_point[0]
+    dpy = destination_point[1]
+
+    # boxes that holds source and destination cords
     src_box = None
     dst_box = None
+
+    # identify source and destination box
     for box in mesh['boxes']:
-        print(box)
+        # kill loop if both found
+        if src_box != None and dst_box != None:
+            break
+        # check if current box is source box
+        if (box[0] <= spx and box[1] >= spx) and (box[2] <= spy and box[3] >= spy):
+            src_box = box
+        # check if current box is destination box
+        if (box[0] <= dpx and box[1] >= dpx) and (box[2] <= dpy and box[3] >= dpy):
+            dst_box = box
+    
+    # adding keys and adj
+    boxes[src_box] = mesh['adj'][src_box]
+    boxes[dst_box] = mesh['adj'][dst_box]
 
     return path, boxes.keys()
